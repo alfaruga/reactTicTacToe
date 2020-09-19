02709891ba78c53cdc9ecdd272ccc8ff426d7046
Alexis Ruiz <alfaruga@gmail.com>
2020-09-19 13:58:31 -0500

    Updates

---
+++ precache-manifest.11cf4fd6ca79f26da2fd90e0244db12a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ad3f1fc9410adadbe7be2abf6c3b8d2",
+    "url": "/reactTicTacToe/index.html"
+  },
+  {
+    "revision": "204014fd8eea4c7c4d52",
+    "url": "/reactTicTacToe/static/css/main.5757f3b0.chunk.css"
+  },
+  {
+    "revision": "a9fc8a10e3be6666dd48",
+    "url": "/reactTicTacToe/static/js/2.518d3c4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactTicTacToe/static/js/2.518d3c4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "204014fd8eea4c7c4d52",
+    "url": "/reactTicTacToe/static/js/main.ebc5487f.chunk.js"
+  },
+  {
+    "revision": "1bc67d9dd40ad03f7588",
+    "url": "/reactTicTacToe/static/js/runtime-main.543940eb.js"
+  },
+  {
+    "revision": "1f7684f78c4b2ac99d35604ba22a6fa9",
+    "url": "/reactTicTacToe/static/media/bgimg.1f7684f7.jpg"
+  }
+]);
